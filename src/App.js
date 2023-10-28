0ae6c06b284ee6e7136304e6f3a7730f3db09df4
unknown <naazfalak5119@gmail.com>
2023-10-28 15:22:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Signup from "./components/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogIn from "./components/LogIn";
+import ViewTable from "./components/ViewTable";
+import EditPage from "./components/EditPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/view" element={<ViewTable />} />
+        <Route path="/edit/:id" element={<EditPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
